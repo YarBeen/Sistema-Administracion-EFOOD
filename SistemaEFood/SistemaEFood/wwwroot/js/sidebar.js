e19237d080a66e2675b70c5a48bad80813e41f31
YarBeen <ycharpentier@estudiantec.cr>
2024-05-29 12:47:50 -0600

    SDVD-68 Side bar

---
+++ SistemaEFood/SistemaEFood/wwwroot/js/sidebar.js
@@ -0,0 +1,7 @@
+﻿function openSidebar() {
+    document.getElementById("nav-bar-menu").style.width = "250px";
+}
+
+function closeSidebar() {
+    document.getElementById("nav-bar-menu").style.width = "0";
+}

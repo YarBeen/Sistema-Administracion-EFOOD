d95b4660208db39ba3abddd0c5ca6f25f9a996ec
Yosward García <yosward.gar@gmail.com>
2024-06-02 00:37:37 -0600

    Creación del proyecto de vista del cliente

---
+++ SistemaEFood/EFood/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace EFood.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}

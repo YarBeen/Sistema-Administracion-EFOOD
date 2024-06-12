5d50a1a311612cde9820be1363c5a76f2eea7149
Yosward García <yosward.gar@gmail.com>
2024-06-12 16:33:01 -0600

    SDVD-33 Filttrado de pedidos por fecha y por estado en la seccion de administrador al hacer consultas

---
+++ SistemaEFood/SistemaEFood/Areas/Admin/Controllers/PedidoController.cs
@@ -0,0 +1,110 @@
+﻿using Microsoft.AspNetCore.Mvc.Rendering;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+using SistemaEFood.AccesoDatos.Repositorio.IRepositorio;
+using SistemaEFood.Modelos;
+using SistemaEFood.Modelos.ViewModels;
+using SistemaEFood.Utilidades;
+using SistemaEFood.Servicios;
+
+namespace SistemaEFood.Areas.Admin.Controllers
+{
+    [Area("Admin")]
+    [Authorize(Roles = DS.Role_Admin + "," + DS.Role_Mantenimiento)]
+    
+    public class PedidoController : Controller
+    {
+       
+        private readonly IUnidadTrabajo _unidadTrabajo;
+        private readonly IWebHostEnvironment _webHostEnviroment;
+        private readonly IStorageService _storageService;
+
+        public PedidoController(IUnidadTrabajo unidadTrabajo, IWebHostEnvironment webHostEnviroment, IStorageService storageService)
+        {
+            
+            _unidadTrabajo = unidadTrabajo;
+            _webHostEnviroment = webHostEnviroment;
+            _storageService = storageService;
+        }
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        
+        public async Task<IActionResult> Consultar()
+        {
+            PedidoVM pedidoVM = new PedidoVM()
+            {
+                ListaOrdenDetalles = await _unidadTrabajo.OrdenDetalle.ObtenerTodos()
+            };
+            return View(pedidoVM);
+        }
+
+
+
+
+        #region API
+        [HttpGet]
+        public async Task<IActionResult> ObtenerTodos()
+        {
+            var todos = await _unidadTrabajo.OrdenDetalle.ObtenerTodos();
+            return Json(new { data = todos });
+        }
+
+        [HttpGet]
+        public async Task<IActionResult> ObtenerPorFecha(DateTime fecha)
+        {
+            if (fecha == DateTime.MinValue)
+            {
+                var todos = await ObtenerTodos();
+                return todos;
+            }
+            var registrosBitacora = await _unidadTrabajo.OrdenDetalle.ObtenerPorFecha(fecha);
+            return Json(new { data = registrosBitacora });
+
+        }
+
+        [HttpGet]
+        public async Task<IActionResult> ConsultarConFiltro(DateTime fechainicial, DateTime fechafinal, string estado)
+        {
+            IEnumerable<OrdenDetalle> registrosOrdenes;
+
+            if (fechainicial != DateTime.MinValue && fechafinal != DateTime.MinValue)
+            {
+                registrosOrdenes = await _unidadTrabajo.OrdenDetalle.ObtenerEntreFechas(fechainicial, fechafinal);
+
+                if (!string.IsNullOrEmpty(estado) && estado != "-- Seleccione un estado --")
+                {
+                    registrosOrdenes = registrosOrdenes.Where(p => p.Estado == estado);
+                }
+            }
+            else
+            {
+                registrosOrdenes = await _unidadTrabajo.OrdenDetalle.ObtenerTodos();
+
+                if (!string.IsNullOrEmpty(estado) && estado != "-- Seleccione un estado --")
+                {
+                    registrosOrdenes = registrosOrdenes.Where(p => p.Estado == estado);
+                }
+            }
+
+            return Json(new { data = registrosOrdenes });
+        }
+
+        [HttpGet]
+        public async Task<IActionResult> ObtenerPorEstado(string estado)
+        {
+            if (string.IsNullOrEmpty(estado))
+            {
+                return await ObtenerTodos();
+            }
+
+            var pedidos = await _unidadTrabajo.OrdenDetalle.ObtenerPedidosPorEstado(estado);
+            return Json(new { data = pedidos });
+        }
+
+        #endregion
+
+    }
+}
---
+++ SistemaEFood/SistemaEFood/wwwroot/js/pedido.js
@@ -0,0 +1,71 @@
+﻿let datatable;
+
+$(document).ready(function () {
+    loadDataTable();
+
+    $('#btnBuscar').on('click', function () {
+        filterDataTable();
+    });
+
+    $('#btnLimpiarFiltro').on('click', function () {
+        clearDataTable();
+    });
+});
+
+
+function loadDataTable() {
+    datatable = $('#tblDatos').DataTable({
+        "language": {
+            "lengthMenu": "Mostrar _MENU_ Registros Por Página",
+            "zeroRecords": "Ningún Registro",
+            "info": "Mostrar página _PAGE_ de _PAGES_",
+            "infoEmpty": "no hay registros",
+            "infoFiltered": "(filtrado de _MAX_ registros totales)",
+            "search": "Buscar",
+            "paginate": {
+                "first": "Primero",
+                "last": "Último",
+                "next": "Siguiente",
+                "previous": "Anterior"
+            }
+        },
+        "ajax": {
+            "url": "/Admin/Pedido/ConsultarConFiltro",
+            "data": function (d) {
+                d.fechainicial = $('#fechaInicio').val();
+                d.fechafinal = $('#fechaFin').val();
+                d.estado = $('#estado').val();
+            }
+        },
+        "columns": [
+            { "data": "id", "width": "20%" },
+            {
+                "data": "fechaOrden", "width": "40%",
+                "render": function (data) {
+                    var fecha = new Date(data);
+                    var opciones = { year: 'numeric', month: 'short', day: 'numeric' };
+                    return fecha.toLocaleDateString('es-ES', opciones);
+                }
+            },
+            { "data": "estado", "width": "20%" },
+            {
+                "data": "monto", "width": "20%",
+                "render": function (data) {
+                    return '₡' + parseFloat(data).toFixed(2);
+                }
+            }
+        ]
+    });
+}
+
+
+function filterDataTable() {
+    datatable.ajax.reload();
+}
+
+function clearDataTable() {
+    $('#fechaInicio').val('');
+    $('#fechaFin').val('');
+    $('#estado').val('');
+    datatable.ajax.reload();
+}

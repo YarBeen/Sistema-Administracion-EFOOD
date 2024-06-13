75ec4e76a395e5fba019eba28997a74752f2d09f
Yosward García <yosward.gar@gmail.com>
2024-06-13 17:20:27 -0600

    Eliminacion de archivos y carpetas sin utilizar

--- SistemaEFood/EFood/Models/ErrorViewModel.cs
+++
@@ -1,9 +0,0 @@
-namespace EFood.Models
-{
-    public class ErrorViewModel
-    {
-        public string? RequestId { get; set; }
-
-        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
-    }
-}
